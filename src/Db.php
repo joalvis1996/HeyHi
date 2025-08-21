1d4b773d1a33936f5e6e2362a34c0d6c98a32f2b
alvisjo <lwalwakk1@naver.com>
2025-08-21 18:14:03 +0900

    DB 수정

--- src/Db.php
+++ src/Db.php
@@ -1,16 +1,24 @@
 <?php
-namespace HeyHi;
+require __DIR__ . '/vendor/autoload.php';
 
-use PDO;
+use Dotenv\Dotenv;
 
-class Db {
-    public static function connect(): PDO {
-        $dsn = sprintf(
-            'pgsql:host=%s;port=%s;dbname=%s',
-            $_ENV['DB_HOST'],
-            $_ENV['DB_PORT'],
-            $_ENV['DB_NAME']
-        );
-        return new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
-    }
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $dsn = sprintf(
+        "pgsql:host=%s;port=%s;dbname=%s",
+        $_ENV['DB_HOST'],
+        $_ENV['DB_PORT'],
+        $_ENV['DB_NAME']
+    );
+
+    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
 }
